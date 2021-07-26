b7416e987aa427775c1abd7182f7f27818e68ce8
Matheus Sabatine Lima <matheussabatine1717@gmail.com>
2021-07-26 15:38:01 -0300

    dia 26/07/2021 Segundo commit

--- SIDEBAR 2 TESTES/comando.js
+++ SIDEBAR 2 TESTES/comando.js
@@ -1,19 +1,23 @@
+/*botão serve para guardar e revelar navbar + animação da seta*/ 
 $('.botao').click(function(){
     $(this).toggleClass("click");
     $('span').toggleClass("rotater");
     $('.sidebar').toggleClass("show");
 });
 
+/*da a funcionalidade de abrir e fechar a pasta masculina*/
 $('.pasta-homem').click(function(){
     $('nav ul .moda-homem').toggleClass("show");
     $('nav ul .seta-homem').toggleClass("rotate");
 });
 
+/*da a funcionalidade de abrir e fechar a pasta feminina*/
 $('.pasta-mulher').click(function(){
     $('nav ul .moda-mulher').toggleClass("show");
     $('nav ul .seta-mulher').toggleClass("rotate");
 });
 
+/*caso um item já esteja selecionado/ativado, ele será destivado quando um novo item for selecionado/ativado,*/
 $('nav ul li').click(function(){
     $(this).addClass("active").siblings().removeClass("active");
 
