4b61cbe92f12689a199f550a6eee5a1e89699fa5
matheussabatine <matheussabatine1717@gmail.com>
2021-07-29 23:04:20 -0300

    dia 29/07/2021 fazer layout de produtos e modal de carrinho

--- SIDEBAR 2 TESTES/comando.js
+++ SIDEBAR 2 TESTES/comando.js
@@ -1,17 +1,18 @@
-/*botão serve para guardar e revelar navbar + animação da seta*/ 
+/*botão para guardar e revelar navbar + animação da seta*/ 
 $('.botao').click(function(){
     $(this).toggleClass("click");
-    $('span').toggleClass("rotater");
-    $('.sidebar').toggleClass("show");
+    $('span').toggleClass("roda-seta");
+    $('.sidebar').toggleClass("moverSB");
 });
 
-/*da a funcionalidade de abrir e fechar a pasta masculina*/
+/*Funcionalidade de abrir e fechar a pasta masculina + girar seta da pasta
+SHOW= mostra conteúdo da pasta ROTATE= roda seta da pasta*/
 $('.pasta-homem').click(function(){
     $('nav ul .moda-homem').toggleClass("show");
     $('nav ul .seta-homem').toggleClass("rotate");
 });
 
-/*da a funcionalidade de abrir e fechar a pasta feminina*/
+/*Funcionalidade de abrir e fechar a pasta feminina + girar seta da pasta*/
 $('.pasta-mulher').click(function(){
     $('nav ul .moda-mulher').toggleClass("show");
     $('nav ul .seta-mulher').toggleClass("rotate");
